a1920e7398ebf5c50980c13dfb570bb0d7785315
璐璐 <lulu@ua-unistudent-ten-9-194-68.uniaccess.unimelb.edu.au>
2015-08-28 16:42:12 +1000

    yiming commits

--- comp90018/ViewController.swift
+++ comp90018/ViewController.swift
@@ -18,6 +18,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //test
+        //hello
+        //test123
+        //heeeeeeyy
+        
         //teeeeeessssttt
     }
 
