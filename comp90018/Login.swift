bdbbf7109aa0fdfb8213d8f53871b2ca9549a70c
Pramudita Santoso <parfaire@192-168-1-105.tpgi.com.au>
2015-09-28 14:19:59 +1000

    installed cocoapods

--- comp90018/Login.swift
+++ comp90018/Login.swift
@@ -10,23 +10,29 @@ import UIKit
 import Alamofire
 
 let clientId = "085bfe1ee3c54cc886077ba51a5b8d7b"
-let token = "2208426948.085bfe1.fb1f04160fd64355a77faecb23a2fc68"
+let access_token = "2208426948.085bfe1.fb1f04160fd64355a77faecb23a2fc68"
 let redirectUri = "http://backendlife.com"
-
 class Login: UIViewController {
     @IBOutlet weak var webView: UIWebView!
-    @IBOutlet weak var lblTest: UILabel!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var ivProfPict: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //libraries
         let lib = Libraries()
-        //lblTest.text = "1"
+        let user = Libraries.User(libraries:lib)
+        
+        //check token
         let request = lib.login(clientId,redirectUri: redirectUri)
         webView.loadRequest(request)
-        /*Alamofire.request(.GET, "https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code", parameters: ["foo": "bar"])
-        .response { request, response, data, error in
-        print(response)
-        }*/
+        
+        //getUser and the details
+        user.getProfile(access_token)
+        lblName.text = user.name
+        let url = NSURL(string: user.profPict)
+        let data = NSData(contentsOfURL: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check
+        ivProfPict.image = UIImage(data: data!)
     }
     
     override func didReceiveMemoryWarning() {
