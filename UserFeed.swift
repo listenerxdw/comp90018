7084c1a3c98fb505a12b2f683339b205e679ee57
Pramudita <parfaire@ua-unistudent-ten-9-250-63.uniaccess.unimelb.edu.au>
2015-10-07 21:08:23 +1100

    Profile done and logout moved

---
+++ UserFeed.swift
@@ -0,0 +1,13 @@
+//
+//  UserFeed.swift
+//  comp90018
+//
+//  Created by Pramudita on 7/10/2015.
+//  Copyright (c) 2015 Pramudita. All rights reserved.
+//
+
+import UIKit
+
+class UserFeed: UICollectionViewCell {
+    
+}
